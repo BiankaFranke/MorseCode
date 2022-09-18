b22857d73206dccaf51c200b5605afc8ce1f93cf
“BiankaFranke” <bianka_franke@hotmail.de>
2022-09-18 12:45:23 +0200

    upload morse code

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+// JS Check
+console.log('it works');
+
+// Declaration
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+];
+
+let btn = document.querySelector('#submitBtn');
+
+// Function to encrypt
+
+let morse = (event) => {
+    event.preventDefault();
+    let result = '';
+    let input = document.querySelector('#inputMessage').value;
+    let Input = input.toUpperCase();
+    let output = document.querySelector('#output');
+
+    for (let i = 0; i < Input.length; i++) {
+        for (let j = 0; j < morseAlphabet.length; j++) {
+            if (Input[i] == morseAlphabet[j].letter) {
+                result += `${morseAlphabet[j].morseCode} `;
+            }
+        }
+    }
+    output.innerHTML = result;
+}
+
+// Eventlistener
+btn.addEventListener('click', morse);
